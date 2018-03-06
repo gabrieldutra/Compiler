c63bb3abc1b72d4b54f592c74af38f05e05f4095
Aluno <nesk.frz@gmail.com>
2018-03-06 15:56:33 -0300

    Código inicial

---
+++ src/lexical/LexicalAnalysis.java
@@ -0,0 +1,39 @@
+package lexical;
+
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.PushbackInputStream;
+
+import java.util.*;
+
+public class LexicalAnalysis implements AutoCloseable {
+
+    private int line;
+    private SymbolTable st;
+    private PushbackInputStream input;
+
+    public LexicalAnalysis(String filename) throws LexicalException {
+        try {
+            input = new PushbackInputStream(new FileInputStream(filename));
+        } catch (Exception e) {
+            throw new LexicalException("Unable to open file");
+        }
+
+        st = new SymbolTable();
+        line = 1;
+    }
+
+    public void close() throws IOException {
+        input.close();
+    }
+
+    public int getLine() {
+        return this.line;
+    }
+
+    public Lexeme nextToken() throws IOException {
+         Lexeme lex = new Lexeme("", TokenType.END_OF_FILE);
+
+         return lex;
+    }
+}
