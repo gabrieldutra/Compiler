52bf12d5d9c6e3c098fe0662326c15d490c1f22f
Marcelo Ferreira Cândido <marmacedoo9@gmail.com>
2018-03-11 16:36:37 -0300

    Nomeando os tipos de token a serem captados pela parte léxica do interpretador através da estrutura de enumeração TokenType.

--- src/lexical/TokenType.java
+++ src/lexical/TokenType.java
@@ -7,12 +7,33 @@ public enum TokenType {
     END_OF_FILE,
 
     // symbols
-
+    DOT_COMMA,   // ';'
+    COMMA,       // ','
+    OPEN_CUR,    // '{'
+    CLOSE_CUR,   // '}'
+    OPEN_PAR,    // '('
+    CLOSE_PAR,   // ')'
+    
     // keywords
-
-
+    FUCTION,    // fuction def
+    
     // operators
-
+    DOT,        // '.'
+    ATTRIB,     // '='
+    PLUS,       // '+'
+    MINUS,      // '-'
+    MULT,       // '*'
+    DIV,        // '/'
+    MOD,        // '%'
+    LESS,       // '<'
+    GREATER,    // '>'
+    LESS_EQ,    // '>='
+    GREATER_EQ, // '<='
+    NOT_EQ,     // '!='
+    NOT,        // '!'
+    AND,        // '&'
+    OR,         // '|'
+    
     // others
-
+    NAME
 };
